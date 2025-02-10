a95573fbe6f3b66c211203ee0faba385ea8c3cec
Mark <madlyinanger37@gmail.com>
2025-02-09 22:57:22 -0600

    fix: remove required modifier from Id property in TaskItem model

--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -2,7 +2,7 @@ namespace TodoApi.Models;
 
 public sealed class TaskItem
 {
-    public required Guid Id { get; init; }
+    public Guid Id { get; init; }
     public required string Name { get; init; }
     public required DateTime DueDate { get; init; }
 }
